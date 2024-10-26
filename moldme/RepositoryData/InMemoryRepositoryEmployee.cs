a6930cff2280e9b63f742bc17f75ff8a8483887e
LEODESK\abcdt <8210127@estg.ipp.pt>
2024-10-26 17:19:21 +0100

    Employee adicionado à dev

---
+++ moldme/RepositoryData/InMemoryRepositoryEmployee.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DefaultNamespace
+{
+    public class InMemoryRepositoryEmployee
+    {
+        private readonly List<Employee> _employees = new();
+
+        public void AddEmployee(Employee employee)
+        {
+            _employees.Add(employee); 
+        }
+
+        public void EditEmployee(int employeeId, Employee updatedEmployee)
+        {
+            var index = _employees.FindIndex(e => e.EmployeeId == employeeId);
+            if (index != -1)
+            {
+                _employees[index] = updatedEmployee; 
+            }
+        }
+        public void RemoveEmployee(int employeeId)
+        {
+            _employees.RemoveAll(e => e.EmployeeId == employeeId); 
+        }
+
+        public Employee GetEmployeeById(int employeeId)
+        {
+            return _employees.FirstOrDefault(e => e.EmployeeId == employeeId); 
+        }
+
+        public List<Employee> ListAllEmployees()
+        {
+            return _employees; 
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
