ca850eaf0b9dd74a3bf0896a869ff69f8501fa0f
8210136 <8210136@estg.ipp.pt>
2024-10-27 17:06:38 +0000

    Models update

--- moldme/Models/Employee.cs
+++ moldme/Models/Employee.cs
@@ -1,9 +1,41 @@
-﻿namespace DefaultNamespace;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DefaultNamespace;
 
 public class Employee
 {
-    public int EmployeeId { get; set; }
-    public string EditEmployee { get; set; }
-    public string RemoveEmployee { get; set; }
-    public string ListAllEmployees { get; set; }
+    [Key]
+    [StringLength(6)]
+    public string StaffID { get; set; }
+        
+    [Required]
+    [StringLength(64)]
+    public string Name { get; set; }
+        
+    [Required]
+    [StringLength(64)]
+    public string Profession { get; set; }
+        
+    [Required]
+    [Range(100000000, 999999999)]
+    public int NIF { get; set; }
+        
+    [Required]
+    [StringLength(64)]
+    public string Email { get; set; }
+        
+    [Range(100000000, 999999999)]
+    public int? Contact { get; set; }
+        
+    [Required]
+    [StringLength(64)]
+    public string Password { get; set; }
+        
+    [StringLength(6)]
+    public string CompanyID { get; set; }
+
+    [ForeignKey("CompanyID")]
+    public Company Company { get; set; }
 }
+
---
+++ moldme/Models/Payment.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace moldme.Models;
+
+public class Payment
+{
+    [Key]
+    [StringLength(6)]
+    public string PaymentID { get; set; }
+
+    [Required]
+    public float Value { get; set; }
+
+    [Required]
+    [DataType(DataType.Date)]
+    public DateTime Date { get; set; }
+
+    [Required]
+    public SubscriptionPlan Plan { get; set; }
+}
