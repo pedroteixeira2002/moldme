c71583c857563372b8693f435a3db10de9869634
8210136 <8210136@estg.ipp.pt>
2024-11-06 15:07:08 +0000

    Alterações de logica do pagamento

--- moldme/Models/SubscriptionPlan.cs
+++ moldme/Models/SubscriptionPlan.cs
@@ -5,4 +5,19 @@ public enum SubscriptionPlan
     Basic,
     Pro,
     Premium,
+    None
+}
+public static class SubscriptionPlanHelper
+{
+    public static float GetPlanPrice(SubscriptionPlan plan)
+    {
+        return plan switch
+        {
+            SubscriptionPlan.None => 0,
+            SubscriptionPlan.Basic => 9.99f,
+            SubscriptionPlan.Pro => 19.99f,
+            SubscriptionPlan.Premium => 29.99f,
+            _ => throw new ArgumentOutOfRangeException(nameof(plan), "Unknown plan type")
+        };
+    }
 }
